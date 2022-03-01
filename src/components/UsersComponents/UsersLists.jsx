d3b5be7626d455ab45784c7988a17c73312043af
fatt-chong <fatt.guillermo.chong.antilef@gmail.com>
2022-03-01 17:24:20 -0300

    se listo a los usuarios

--- src/components/UsersComponents/UsersLists.jsx
+++ src/components/UsersComponents/UsersLists.jsx
@@ -6,24 +6,53 @@ import {
   } from "react-router-dom";
 import React, {useState, useEffect} from "react";
 import axios from 'axios';
+import {Table} from 'react-bootstrap';
 
 const UsersLists = () =>{
+
+    const [user, setUser] = useState([]);
     
     useEffect(()=>{
         console.log("solo carga");
         getUsers();
     }, []);
 
+ 
     //listado de usuarios
     const getUsers = async () => {
 
-        let x = await axios.get("http://localhost:3001/usuarios");
-        console.log(x);
+        let usuarios = await axios.get("http://localhost:3001/usuarios");
+        setUser(usuarios.data);
+        console.log(usuarios.data);
+        
     }
 
     return(
         <>
+        <div className="container">
             <h1>En listar usuarios</h1>
+            <Table striped bordered hover variant="dark">
+            <thead>
+                <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>email</th>
+                </tr>
+            </thead>
+            <tbody>
+            {user.map((data)=>(
+                <tr>
+                <td>{data.id}</td>
+                <td>{data.firstName}</td>
+                <td>{data.lastName}</td>
+                <td>{data.email}</td>
+                </tr>
+            ))}
+
+            </tbody>
+            </Table>
+        </div>
         </>
     );
 }
